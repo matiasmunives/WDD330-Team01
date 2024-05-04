6245a951151404d06aae1e444f255c9cb4a5a4f9
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-05-04 12:08:13 -0500

    add to cart broken solve ig

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,26 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  const existingItemIndex = cartItems.findIndex(
+    (item) => item.Id === product.Id
+  );
+
+  if (existingItemIndex !== -1) {
+    cartItems[existingItemIndex].quantity += 1;
+  } else {
+    cartItems.push({ ...product, quantity: 1 });
+  }
+
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
