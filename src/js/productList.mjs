dd8c6e846cc1a64b9a5f238d4474f3e4753e0930
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-05-15 22:10:02 -0500

    changes step 04

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,7 +1,26 @@
-export default function productList(selector, category) {
+import { getData } from  "./productData.mjs";
+
+// productList.mjs
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=">
+        <img
+        src=""
+        alt="Image of "
+    />
+    <h3 class="card__brand"></h3>
+    <h2 class="card__name"></h2>
+    <p class="product-card__price">$</p></a>
+    </li>`
+    }  
     
-    // get the element we will insert the list into from the selector
-    // get the list of products 
-    // render out the product list to the element
-    
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+const element = document.querySelector(selector);
+
+// get the list of products
+const products = await getData(category);
+console.log(products);
+  // render out the product list to the element
+
 }
