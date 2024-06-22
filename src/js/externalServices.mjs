7891bef747720133395920c77b1bef0eede367b2
Carol Madella <carolina.madella@gmail.com>
2024-06-21 22:31:33 -0400

    undo changes - and fixing image loading

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,22 +1,13 @@
 const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
 
-// async function convertToJson(res) {
-//   const jsonResponse = await res.json();
-//   if (res.ok) {
-//     return res.json();
-//   } else {
-//     throw { name: 'servicesError', message: jsonResponse };
-//   }
-// }
-
-function convertToJson(res) {
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
   if (res.ok) {
-    return res.json();
+    return jsonResponse;
   } else {
-    throw new Error("Bad Response");
+    throw { name: 'servicesError', message: jsonResponse };
   }
 }
-
 
 export async function getProductsByCategory(category) {
   const response = await fetch(baseURL + `products/search/${category}`);
@@ -29,6 +20,7 @@ export async function findProductById(id) {
   const product = await convertToJson(response);
   return product.Result;
 }
+
 export async function checkout(payload) {
   const options = {
     method: "POST",
