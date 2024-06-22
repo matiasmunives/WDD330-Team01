53b267d7849c6eebb21f3c1ddc2f78ee39a5a59a
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-06-21 20:40:22 -0500

    add validation and erro handling first part

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,10 +1,11 @@
 const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
 
-function convertToJson(res) {
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw { name: 'servicesError', message: jsonResponse };
   }
 }
 
