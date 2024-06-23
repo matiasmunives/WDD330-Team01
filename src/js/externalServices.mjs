c3c8fae212b42f55921430d727b814caf314dfef
matiasmunives <mun19005@byui.edu>
2024-06-23 01:01:45 -0300

    activity 8

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -30,3 +30,40 @@ export async function checkout(payload) {
   };
   return await fetch(baseURL + "checkout/", options).then(convertToJson);
 }
+
+export async function loginRequest(creds) {
+  const response = await fetch("http://server-nodejs.cit.byui.edu:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(creds)
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Login failed");
+  }
+
+  const data = await response.json();
+  return data.token;
+}
+
+//getorders
+export async function getOrders(token) {
+  const response = await fetch("http://server-nodejs.cit.byui.edu:3000/orders", {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Failed to fetch orders");
+  }
+
+  const data = await response.json();
+  return data;
+}
